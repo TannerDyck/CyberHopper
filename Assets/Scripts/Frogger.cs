6934598d509054401ff64507f36f6a8cf97dcaee
TannerDyck <97553206+TannerDyck@users.noreply.github.com>
2025-03-19 19:34:13 -0700

    Implemented respawn timer
    Frogger will now respawn at the start position after 1 second of being dead. This will be changed later to allow for game over screens, and world resets. But the main functionality will remain the same.

--- Assets/Scripts/Frogger.cs
+++ Assets/Scripts/Frogger.cs
@@ -7,10 +7,12 @@ public class Frogger : MonoBehaviour
     public Sprite idleSprite;
     public Sprite leapSprite;
     public Sprite deathSprite;
+    private Vector3 spawnPosition;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        spawnPosition = transform.position;
     }
 
     private void Update()
@@ -92,9 +94,23 @@ public class Frogger : MonoBehaviour
 
     private void Death() 
     {
+        StopAllCoroutines();
+
         transform.rotation = Quaternion.identity;
         spriteRenderer.sprite = deathSprite;
         enabled = false;
+
+        Invoke(nameof(Respawn), 1f);
+    }
+
+    private void Respawn()
+    {
+        StopAllCoroutines();
+
+        transform.rotation = Quaternion.identity;
+        transform.position = spawnPosition;
+        spriteRenderer.sprite = idleSprite;
+        enabled = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
