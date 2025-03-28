5dca86dc9b5621cec342b681dbcd4f812be051d2
Soulcraftgaming <alexbatke01@gmail.com>
2025-03-28 00:25:34 -0700

    implemented basic speed boost instance

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour
 {
     [Header("Power-up Settings")]
-    public float invincibilityDuration = 5f;
-    public bool isInvincibilityPowerUp = true;
+    public bool isInvincibilityPowerUp;
+    public bool isSpeedBoostPowerUp;
+    public float duration = 5f;
+    public float speedMultiplier = 5f; // Only for Speed Boost
 
     [Header("Visual Effects")]
-    public GameObject collectEffect; // Optional particle effect
+    public GameObject collectEffect;
 
-    // Private reference to manager
     private PowerUpManager manager;
     private bool isCollected = false;
 
-    // Public setter method for the manager
+    // 🔹 Fix: Add this missing method
     public void SetManager(PowerUpManager manager)
     {
         this.manager = manager;
@@ -21,63 +22,51 @@ public class PowerUp : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Prevent multiple collections
         if (isCollected) return;
 
         if (other.CompareTag("Player"))
         {
-            // Mark as collected to prevent multiple triggers
             isCollected = true;
-            Debug.Log("Player collected power-up!");
-
-            // Apply effects before destroying
             ApplyPowerUpEffect(other.gameObject);
 
-            // Show collection effect if assigned
             if (collectEffect != null)
             {
                 Instantiate(collectEffect, transform.position, Quaternion.identity);
             }
 
-            // Cache manager reference before destruction
-            PowerUpManager managerRef = this.manager;
-
-            // If no manager reference, try to find one
-            if (managerRef == null)
-            {
-                managerRef = FindAnyObjectByType<PowerUpManager>();
-            }
-
-            // Destroy this object
+            PowerUpManager managerRef = this.manager ?? FindAnyObjectByType<PowerUpManager>();
             Destroy(gameObject);
 
-            // After destroying, notify manager (if we have a reference)
             if (managerRef != null)
-            {
                 managerRef.OnPowerUpCollected();
-            }
             else
-            {
                 Debug.LogWarning("No PowerUpManager found!");
-            }
         }
     }
 
-    private void ApplyPowerUpEffect(GameObject player)
+    public void ApplyPowerUpEffect(GameObject player)
     {
+        Debug.Log("ApplyPowerUpEffect called for " + player.name);
+        Frogger frogController = player.GetComponent<Frogger>();
+        if (frogController == null)
+        {
+            Debug.LogError("Player object doesn't have a Frogger component!");
+            return;
+        }
+
         if (isInvincibilityPowerUp)
         {
-            // Get the Frogger component and activate invincibility
-            Frogger frogController = player.GetComponent<Frogger>();
-            if (frogController != null)
-            {
-                frogController.ActivateInvincibility(invincibilityDuration);
-                Debug.Log("Invincibility activated for " + invincibilityDuration + " seconds!");
-            }
-            else
-            {
-                Debug.LogError("Player object doesn't have a Frogger component!");
-            }
+            frogController.ActivateInvincibility(duration);
+            Debug.Log("Invincibility activated for " + duration + " seconds!");
+        }
+        else if (isSpeedBoostPowerUp)
+        {
+            frogController.ActivateSpeedBoost(speedMultiplier, duration);
+            Debug.Log("Speed Boost activated! Multiplier: " + speedMultiplier);
+        }
+        if (manager != null)
+        {
+            manager.OnPowerUpCollected();
         }
     }
 }
